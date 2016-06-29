0735dfc0d685b5ac929485f24d88ce7bae941066
Aparna-Gopi <aparna.gopi56@gmail.com>
2016-06-29 18:38:30 +0530

    Adding first sorting algo.

---
+++ sorting/insertion.py
@@ -0,0 +1,9 @@
+a = raw_input()
+a = a.split(",")
+arr = [eval(x) for x in a]
+for i in range(1,len(arr)) :
+	for j in range(i-1, -1, -1) :
+		if arr[j] > arr[i]:
+			arr[j], arr[i] = arr[i], arr[j]
+			i = i-1
+print arr
